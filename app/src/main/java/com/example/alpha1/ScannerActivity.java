9478e12ded5209fc82d98dc1813924ea3d21bc75
ab2916-Erel <erel10bb@gmail.com>
2024-03-18 08:21:46 +0200

    siyamti

--- app/src/main/java/com/example/alpha1/ScannerActivity.java
+++ app/src/main/java/com/example/alpha1/ScannerActivity.java
@@ -1,8 +1,11 @@
 package com.example.alpha1;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +14,12 @@ import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class ScannerActivity extends AppCompatActivity {
     Button scan_btn;
     TextView  textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +31,42 @@ public class ScannerActivity extends AppCompatActivity {
         scan_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                scanCode();
                 IntentIntegrator intentIntegrator = new IntentIntegrator(ScannerActivity.this);
                 intentIntegrator.setOrientationLocked(true);
                 intentIntegrator.setPrompt("Scan a QR Code");
                 intentIntegrator.setDesiredBarcodeFormats(intentIntegrator.QR_CODE);
                 intentIntegrator.initiateScan();
-
             }
         });
     }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() != null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(ScannerActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+
+                }
+            }).show();
+
+        }
+    });
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
